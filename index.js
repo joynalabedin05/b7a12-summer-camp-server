9676e29b2848ca1fd1cfdf845acc307c3341f849
joynalabedin05 <joynal05101993@gmail.com>
2023-06-09 19:53:45 +0600

    payment added

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const jwt = require('jsonwebtoken');
 const app = express();
 require('dotenv').config();
 
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
+
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -48,6 +50,7 @@ async function run() {
     const classCollection = client.db('summerCamp').collection('classes');
     const instructorCollection = client.db('summerCamp').collection('instructor');
     const cartCollection = client.db('summerCamp').collection('carts');
+    const paymentCollection = client.db('summerCamp').collection('payments');
 
 
      // veryfy admin
@@ -177,6 +180,28 @@ async function run() {
         const result = await cartCollection.deleteOne(query);
         res.send(result);
       });
+
+      // create payment intent
+    app.post('/create-payment-intent',verifyJWT, async(req,res)=>{
+      const {price} = req.body;
+      const amount = parseInt(price*100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      })
+    });
+
+    app.post('/payments',verifyJWT, async(req, res)=>{
+      const payment = req.body;
+      const insertResult = await paymentCollection.insertOne(payment);
+      const query = {_id: {$in: payment.cartItems.map(id=>new ObjectId(id))}};
+      const deleteResult = await cartCollection.deleteMany(query);
+      res.send({insertResult, deleteResult});
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
